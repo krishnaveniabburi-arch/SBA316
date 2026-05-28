59c59f6d3d21e514f454b5ee57901db5adbef9aa
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-05-28 13:45:41 -0400

    newvalidation logic

--- script.js
+++ script.js
@@ -7,4 +7,18 @@ const bookReview = document.getElementById('bookReview');
 const bookShelf = document.querySelector('#bookShelf');
 const checkShelfBtn = document.querySelector('.info-btn');
 
-// using event Listeners
+// using 3 types of  event Listeners input and submit
+bookForm.addEventListener('submit', handleFormSubmit);
+bookReview.addEventListener('input', runLiveReviewValidation); 
+checkShelfBtn.addEventListener('click, showShelfStatitics');
+
+// Event handler functions & validation
+function handleFormSubmit(event)
+  Event.preventDefault(); // page refresh
+  
+// html attribute validation check
+let isHtmlValid = bookTitle.validity.valid;
+if (!isHtmlValid) {
+    titleValidationHint.style.display = 'block';
+    bookTitle.style.borderColor = '#b91c1c';
+}
