0dc4655c05d0a74fbfb0be326061fc20b4ba3794
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-05-28 12:20:01 -0400

    add script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// caching elements using getElementById
+const bookForm = document.getElementById('bookForm');
+const bookTitle = document.getElementById('bookTitle');
+const bookReview = document.getElementById('bookReview');
