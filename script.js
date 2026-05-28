0d587324b17886c846d1cecac47e2b5ff9de17be
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-05-28 12:37:54 -0400

    newvalidation logic

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-// caching elements using getElementById
+// cache  elements using getElementById
 const bookForm = document.getElementById('bookForm');
 const bookTitle = document.getElementById('bookTitle');
 const bookReview = document.getElementById('bookReview');
+
+// cache elements using query selector
+const bookShelf = document.querySelector('#bookShelf');
+const checkShelfBtn = document.querySelector('.info-btn');
+
+// using event Listeners
